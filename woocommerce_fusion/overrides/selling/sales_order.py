44388ef21db6ecdb427d2e28fad2d98d8f0bf29d
Dirk van der Laarse <dirk@laarse.co.za>
2023-09-17 11:33:04 +0000

    fix: handle scenario where sync is disabled

--- woocommerce_fusion/overrides/selling/sales_order.py
+++ woocommerce_fusion/overrides/selling/sales_order.py
@@ -1,6 +1,7 @@
 import json
 
 import frappe
+from frappe import _
 
 from woocommerce_fusion.woocommerce.doctype.woocommerce_order.woocommerce_order import (
 	generate_woocommerce_order_name_from_domain_and_id,
@@ -38,9 +39,34 @@ def get_woocommerce_order(woocommerce_site, woocommerce_id):
 	"""
 	Retrieves a specific WooCommerce order based on its site and ID.
 	"""
+	# First verify if the WooCommerce site exits, and it sync is enabled
 	wc_order_name = generate_woocommerce_order_name_from_domain_and_id(
 		woocommerce_site, woocommerce_id
 	)
+	wc_additional_settings = frappe.get_cached_doc("WooCommerce Additional Settings")
+	wc_server = next(
+		(
+			server
+			for server in wc_additional_settings.servers
+			if woocommerce_site in server.woocommerce_server_url
+		),
+		None,
+	)
+
+	if not wc_server:
+		frappe.throw(
+			_(
+				"This Sales Order is linked to WooCommerce site '{0}', but this site can not be found in 'WooCommerce Additional Settings'"
+			).format(woocommerce_site)
+		)
+
+	if not wc_server.enable_sync:
+		frappe.throw(
+			_(
+				"This Sales Order is linked to WooCommerce site '{0}', but Synchronisation for this site is disabled in 'WooCommerce Additional Settings'"
+			).format(woocommerce_site)
+		)
+
 	wc_order = frappe.get_doc({"doctype": "WooCommerce Order", "name": wc_order_name})
 	wc_order.load_from_db()
 	return wc_order
