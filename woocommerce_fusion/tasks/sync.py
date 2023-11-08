5b362f39beb5d1574bb5fcebe15824d556fac80f
Dirk van der Laarse <dirk@laarse.co.za>
2023-11-08 07:26:45 +0000

    chore: add comment for debugging

--- woocommerce_fusion/tasks/sync.py
+++ woocommerce_fusion/tasks/sync.py
@@ -271,6 +271,8 @@ def create_and_link_payment_entry(wc_order, sales_order_name):
 				row.allocated_amount = sales_order.grand_total
 				payment_entry.save()
 
+				payment_entry.add_comment("Comment", frappe._("WC Order Metadata: {0}").format(str(meta_data)))
+
 				# Link created Payment Entry to Sales Order
 				sales_order.woocommerce_payment_entry = payment_entry.name
 				sales_order.save()
